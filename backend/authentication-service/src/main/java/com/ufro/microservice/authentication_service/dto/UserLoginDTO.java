79531685acdb72af804defc0830a4a3dccef23d1
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-10 02:16:12 -0300

    feat: add login DTO

---
+++ backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/dto/UserLoginDTO.java
@@ -0,0 +1,27 @@
+package com.ufro.microservice.authentication_service.dto;
+
+public class UserLoginDTO {
+    private String email;
+    private String password;
+
+    public UserLoginDTO(String username, String password) {
+        this.email = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String username) {
+        this.email = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
