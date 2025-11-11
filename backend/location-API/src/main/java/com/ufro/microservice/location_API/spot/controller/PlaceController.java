bdd674fe4a85ceae0896f1d285b3eeacc42e66c0
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-11 14:28:17 -0300

    feat: add place controller

---
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/controller/PlaceController.java
@@ -0,0 +1,9 @@
+package com.ufro.microservice.location_API.spot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("inclusive/api/v1/place/")
+@RestController
+public class PlaceController {
+}
