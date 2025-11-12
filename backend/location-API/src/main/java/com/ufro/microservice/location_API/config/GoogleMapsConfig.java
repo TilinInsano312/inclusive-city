0f8a420862aa7b2ecb820fc092d625ea353d28ea
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-12 03:51:26 -0300

    feat: add google api key config

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/config/GoogleMapsConfig.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/config/GoogleMapsConfig.java
@@ -1,5 +1,6 @@
 package com.ufro.microservice.location_API.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "google.maps")
 public class GoogleMapsConfig {
 
+    @Value("${google.maps.api.key}")
     private String apiKey;
 
-    // Getters y Setters
     public String getApiKey() {
         return apiKey;
     }
