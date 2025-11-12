7dae18105bf90e2c6fbde59d7785fb3c353989b6
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-11 21:13:49 -0300

    feat: add get details controller

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/controller/PlaceController.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/controller/PlaceController.java
@@ -1,9 +1,25 @@
 package com.ufro.microservice.location_API.spot.controller;
 
+import com.ufro.microservice.location_API.common.ApiResponse;
+import com.ufro.microservice.location_API.spot.dto.PlaceDetailDTO;
+import com.ufro.microservice.location_API.spot.service.IPlaceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("inclusive/api/v1/place/")
 @RestController
 public class PlaceController {
+    private final IPlaceService placeService;
+
+    public PlaceController(IPlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("{placeId}")
+    public ResponseEntity<ApiResponse<PlaceDetailDTO>> getPlaceById(@PathVariable String placeId) {
+        return ResponseEntity.ok().body(new ApiResponse<>(placeService.getPlaceDetails(placeId)));
+    }
 }
