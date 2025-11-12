541b273856d8f0e3214d375ebfdb55c328a96e94
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-12 03:50:15 -0300

    feat: add interface place service

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/service/IPlaceService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/service/IPlaceService.java
@@ -1,12 +1,14 @@
 package com.ufro.microservice.location_API.spot.service;
 
-import com.google.maps.GeoApiContext;
+import com.ufro.microservice.location_API.spot.dto.PhotoDTO;
 import com.ufro.microservice.location_API.spot.dto.PlaceDetailDTO;
 import com.ufro.microservice.location_API.spot.dto.PlaceSearchDTO;
+import java.util.List;
 
 public interface IPlaceService {
 
 
     PlaceDetailDTO getPlaceDetails(String placeId);
-    PlaceSearchDTO getPlaceBySearch(GeoApiContext geoApiContext, String query);
+    List<PlaceSearchDTO> getPlaceBySearch(String query);
+    PhotoDTO getPlacePhoto(String photoReference, Integer maxWidth);
 }
