2b06dbd1344518d2d9dc90ebb92a36a83939b8d4
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-11-15 00:52:31 -0300

    feat: add methods for fetching place details and nearby places in PlaceRemoteDataSource

--- frontend/lib/features/map/data/datasources/place_remote_data_source.dart
+++ frontend/lib/features/map/data/datasources/place_remote_data_source.dart
@@ -1,5 +1,10 @@
+import 'package:inclusivecity_frontend/features/map/data/models/place_detail_model.dart';
+
 import '../models/place_model.dart';
 
 abstract class PlaceRemoteDataSource {
   Future<List<PlaceModel>> getPlaceSuggestions(String query);
+
+  Future<PlaceDetailsModel> getPlaceDetails(String placeId);
+  Future<List<PlaceDetailsModel>> getNearbyPlaces(double lat, double lng, int radius);
 }
