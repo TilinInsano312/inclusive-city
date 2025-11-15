11012ccd985aa00e418616d5ef964886098b0a25
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-11-15 00:52:50 -0300

    refactor: simplify PlaceModel by removing unused JSON serialization methods and updating backend parsing

--- frontend/lib/features/map/data/models/place_model.dart
+++ frontend/lib/features/map/data/models/place_model.dart
@@ -1,31 +1,16 @@
-import 'package:json_annotation/json_annotation.dart';
 import 'package:inclusivecity_frontend/features/map/domain/entities/place_suggestion.dart';
 
-part 'place_model.g.dart';
-
-@JsonSerializable()
 class PlaceModel extends PlaceSuggestion {
   const PlaceModel({
     required String placeId,
     required String description,
   }) : super(placeId: placeId, description: description);
 
-  factory PlaceModel.fromJson(Map<String, dynamic> json) {
-    return PlaceModel(
-      placeId: json['place_id'] as String,
-      description: json['description'] as String,
-    );
-  }
-
+  // Factory para backend (location-API)
   factory PlaceModel.fromBackendJson(Map<String, dynamic> json) {
     return PlaceModel(
       placeId: json['placeId'] as String,
-      description: json['address'] as String,
+      description: json['name'] as String, // Backend usa 'name' en search
     );
   }
-
-  factory PlaceModel.fromRootJson(Map<String, dynamic> json) =>
-      _$PlaceModelFromJson(json);
-
-  Map<String, dynamic> toJson() => _$PlaceModelToJson(this);
 }
