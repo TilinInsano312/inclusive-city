13e8b9b9a21d855a3dadeed505ac53af2dd0f7a4
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-17 19:05:22 -0300

    feat: add customSpot methods

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/service/ISpotService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/service/ISpotService.java
@@ -1,11 +1,16 @@
 package com.ufro.microservice.location_API.spot.service;
 
-import com.ufro.microservice.location_API.spot.dto.DTOSpot;
+import com.ufro.microservice.location_API.spot.dto.CustomSpotDTO;
+import com.ufro.microservice.location_API.spot.dto.SaveCustomSpotDTO;
+import com.ufro.microservice.location_API.spot.dto.SpotDTO;
 
 import java.util.List;
 
 public interface ISpotService {
-    DTOSpot insertASpot(DTOSpot spot);
-    List<DTOSpot> getAllSpotsById(String idUser);
+    SpotDTO insertASpot(SpotDTO spot);
+    List<SpotDTO> getAllSpotsById(String idUser);
+    List<CustomSpotDTO> getAllCustomSpotsById(String idUser);
+    CustomSpotDTO insertACustomSpot(CustomSpotDTO customSpotDTO);
+    SaveCustomSpotDTO saveASpotInACustomSpot(SpotDTO spotDTO, String listName);
 
 }
