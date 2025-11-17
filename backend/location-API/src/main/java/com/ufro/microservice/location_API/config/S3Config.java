144ab1dc82ab8e8ccb9dd28a04730a226a67e729
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-17 03:07:13 -0300

    feat: add s3Config

---
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/config/S3Config.java
@@ -0,0 +1,43 @@
+package com.ufro.microservice.location_API.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+
+    @Value("${spring.cloud.aws.s3.endpoint}")
+    private String s3Endpoint;
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String s3AccessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String s3SecretKey;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String s3Region;
+
+    @Bean
+    public S3Client s3Client() {
+        // Credenciales estáticas de R2
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(s3AccessKey, s3SecretKey)
+        );
+
+        return S3Client.builder()
+                .credentialsProvider(credentialsProvider)
+                // Aquí está la clave: apuntamos al endpoint de Cloudflare R2
+                .endpointOverride(URI.create(s3Endpoint))
+                // Región (aunque R2 no la usa, el SDK la pide)
+                .region(Region.of(s3Region))
+                .build();
+    }
+}
