e30e4febbf2090a9ca38842f04d08eff699bceac
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-17 19:03:31 -0300

    chore: rename to spotDTO

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/dto/DTOSpot.java
+++
@@ -1,6 +0,0 @@
-package com.ufro.microservice.location_API.spot.dto;
-
-import com.ufro.microservice.location_API.common.dto.LocationDTO;
-
-public record DTOSpot(String id, String userId, String spotName, String placeId, String address, LocationDTO location, String type) {
-}
