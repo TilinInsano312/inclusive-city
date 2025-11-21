0734996407351e0e59273c711f6fa8457b9f1c4a
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-20 23:00:19 -0300

    feat: add logic for medals and rating

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/place/service/StatDataService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/place/service/StatDataService.java
@@ -2,69 +2,81 @@ package com.ufro.microservice.location_API.place.service;
 
 import com.ufro.microservice.location_API.place.dto.*;
 import com.ufro.microservice.location_API.place.mapper.IPlaceMapper;
+import com.ufro.microservice.location_API.place.mapper.IStatDataMapper;
+import com.ufro.microservice.location_API.place.model.enums.Medals;
 import com.ufro.microservice.location_API.place.repository.IPlaceRepository;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class StatDataService implements IStatDataService {
     private final IPlaceRepository placeRepository;
     private final IPlaceMapper placeMapper;
+    private final IStatDataMapper statDataMapper;
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(StatDataService.class);
 
-    public StatDataService(IPlaceRepository placeRepository, IPlaceMapper placeMapper) {
+    public StatDataService(IPlaceRepository placeRepository, IPlaceMapper placeMapper, IStatDataMapper statDataMapper) {
         this.placeRepository = placeRepository;
         this.placeMapper = placeMapper;
+        this.statDataMapper = statDataMapper;
     }
 
-    //Estadisticas de rating
     @Override
-    public boolean updateStatDataRateChoice(String placeId, String userId, String newRate){
-        calculateAverageRate(placeId);
-        return placeRepository.updateStatDataRateChoice(placeId, userId, newRate);
+    public long updateReview(StatDataResultDTO data, String placeId){
+        return placeRepository.updatePlaceByPlaceId(placeId, data.getFormStatistics(), data.getAverageRate());
     }
     @Override
-    public Map<String, Integer> getRating(String placeId) {
-        AggregatedStatDTO stats = placeRepository.getAggregatedStats(placeId);
-        Map<String, Integer> countsMap = new HashMap<>();
-
-        // Procesar rateCounts
-        if (stats.getRateCounts() != null) {
-            for (StatCountDTO count : stats.getRateCounts()) {
-                if ("LIKE".equals(count.get_id())) {
-                    countsMap.put("LIKES", count.getCount());
-                } else if ("DISLIKE".equals(count.get_id())) {
-                    countsMap.put("DISLIKES", count.getCount());
+    public StatDataResultDTO calculateStatData(String placeId) {
+        PlaceDTO placeDTO = placeMapper.toPlaceDTO(placeRepository.findByPlaceId(placeId).orElseThrow());
+        int total = placeDTO.getStatsData().size();
+        int likeCount =0;
+        Map<Integer, Integer> formsData = new HashMap<>();
+        for (Map.Entry<String, StatDataDTO> entry : placeDTO.getStatsData().entrySet()) {
+            StatDataDTO statDataDTO = entry.getValue();
+            if (statDataDTO.getRateChoice() == "LIKE") {
+                likeCount ++;
+            }
+            for (int i = 0; i <6; i++) {
+                String choice = statDataDTO.getForms().get(i);
+                if (choice.equals("YES")) {
+                    formsData.put(i, formsData.getOrDefault(i, 0) + 1);
                 }
             }
         }
-        countsMap.putIfAbsent("LIKES", 0);
-        countsMap.putIfAbsent("DISLIKES", 0);
-        return countsMap;
+        log.info("Calculated stat data for place ID: " + placeId + " with total responses: " + total);
+        log.info("Like count: " + likeCount + ", Forms data: " + formsData);
+        return new StatDataResultDTO(((float) likeCount / total) * 100, calculateMedals(formsData, total));
     }
+
+    private List<String> calculateMedals(Map<Integer, Integer> formsData, int totalForms) {
+        List<String> formStatistics = new java.util.ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            float percentage = ((float) formsData.getOrDefault(i, 0) / totalForms) * 100;
+            if (percentage > 51) {
+                switch (i) {
+                    case 0 -> formStatistics.add(Medals.ATENCION_PREFERENCIAL.name());
+                    case 1 -> formStatistics.add(Medals.ACCESIBILIDAD.name());
+                    case 2 -> formStatistics.add(Medals.BANOS.name());
+                    case 3 -> formStatistics.add(Medals.BANOS.name());
+                    case 4 -> formStatistics.add(Medals.ESTACIONAMIENTO.name());
+                    case 5  -> formStatistics.add(Medals.FACIL_CIRCULACION.name());
+                }
+            }
+            }
+        log.info("Calculated medals: " + formStatistics);
+        return formStatistics;
+    }
+
     @Override
-    public float calculateAverageRate(String placeId) {
-        Integer likeCount = getRating(placeId).get("LIKES");
-        Integer dislikeCount = getRating(placeId).get("DISLIKES");
-        Integer total = likeCount + dislikeCount;
-        return ((float) likeCount /total)*100;
-    }
-
-    //Formulario
-    @Override
-    public void setForm(String formChoices) {
-
-    }
-
-    @Override
-    public void calculateFormStatistics(String placeId) {
-
-    }
-
-    public StatDataDTO addFormToStatData(StatDataDTO statDataDTO, String placeId) {
-        return null;
+    public long addStatDataToPlace(StatDataDTO statDataDTO, String placeId, String userId) {
+        log.info("Adding stat data to place with ID: " + placeId + " for user: " + userId);
+        PlaceDTO placeDTO = placeMapper.toPlaceDTO(placeRepository.findByPlaceId(placeId).orElseThrow());
+        placeDTO.getStatsData().put(userId, statDataDTO);
+        log.info("Updated stats data for place: " + placeDTO.getStatsData());
+        return placeRepository.updateByPlaceId(placeId, statDataMapper.toStatData(statDataDTO));
     }
 }
