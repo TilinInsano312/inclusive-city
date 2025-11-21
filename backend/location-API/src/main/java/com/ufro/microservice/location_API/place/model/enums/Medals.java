5f3a005b7f7039732dab72ba91c5fb7542dce1e8
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-21 02:53:38 -0300

    feat: add medals enum

---
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/place/model/enums/Medals.java
@@ -0,0 +1,9 @@
+package com.ufro.microservice.location_API.place.model.enums;
+
+public enum Medals {
+    ATENCION_PREFERENCIAL,
+    ACCESIBILIDAD,
+    BANOS,
+    ESTACIONAMIENTO,
+    FACIL_CIRCULACION
+}
