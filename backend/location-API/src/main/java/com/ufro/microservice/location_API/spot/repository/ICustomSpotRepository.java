8b9b926660e98fc5a6b81674e5e266e583d3ffee
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-22 18:22:32 -0300

    feat: add customSpot method

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/repository/ICustomSpotRepository.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/spot/repository/ICustomSpotRepository.java
@@ -1,7 +1,10 @@
 package com.ufro.microservice.location_API.spot.repository;
 
+import com.ufro.microservice.location_API.common.model.Location;
 import com.ufro.microservice.location_API.spot.model.CustomSpot;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,9 @@ import java.util.List;
 public interface ICustomSpotRepository extends MongoRepository<CustomSpot, String> {
     List<CustomSpot> findByUserId(String userId);
     CustomSpot findCustomSpotByUserIdAndListName(String userId, String listName);
+    long deleteCustomSpotByListNameAndUserId(String listName, String userId);
+
+    @Query("{ 'listName': ?0,'userId': ?1}")
+    @Update("{ $pull: { spots: { location: ?2 } } }")
+    long updateCustomSpotByListNameAndUserId(String listName, String userId, Location location);
 }
