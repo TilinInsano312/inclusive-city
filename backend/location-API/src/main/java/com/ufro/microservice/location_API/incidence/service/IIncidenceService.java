75418259ef95a8065659dc2342e00077ac26c072
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-22 18:22:18 -0300

    feat: add incidence method

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/service/IIncidenceService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/service/IIncidenceService.java
@@ -1,10 +1,13 @@
 package com.ufro.microservice.location_API.incidence.service;
 
+import com.ufro.microservice.location_API.common.dto.LocationDTO;
 import com.ufro.microservice.location_API.incidence.dto.IncidenceDTO;
+import com.ufro.microservice.location_API.incidence.dto.IncidenceRequestDTO;
 
 import java.util.List;
 
 public interface IIncidenceService {
-    IncidenceDTO insertAIncidence(IncidenceDTO incidenceDTO);
+    IncidenceDTO insertAIncidence(IncidenceRequestDTO incidenceRequestDTO);
     List<IncidenceDTO> getAllIncidences();
+    List<IncidenceDTO> getIncedenceBySector(LocationDTO pointNorthEast, LocationDTO pointSouthWest);
 }
