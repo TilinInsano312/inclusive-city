d36ff081381db56927857d849a3654361e18dde1
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-22 18:20:53 -0300

    feat: add findbysector controller

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/controller/IncidenceController.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/controller/IncidenceController.java
@@ -2,8 +2,12 @@ package com.ufro.microservice.location_API.incidence.controller;
 
 import com.ufro.microservice.location_API.common.response.ApiResponse;
 import com.ufro.microservice.location_API.incidence.dto.IncidenceDTO;
+import com.ufro.microservice.location_API.incidence.dto.IncidenceRequestDTO;
+import com.ufro.microservice.location_API.incidence.dto.LocationSectorRequest;
 import com.ufro.microservice.location_API.incidence.service.IIncidenceService;
 import com.ufro.microservice.location_API.incidence.service.impl.IncidenceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,19 +16,37 @@ import java.util.List;
 @RestController
 @RequestMapping("inclusive/api/v1/incidence")
 public class IncidenceController {
+    private static final Logger log = LoggerFactory.getLogger(IncidenceController.class);
     private final IIncidenceService incidenceService;
     public IncidenceController(IncidenceService incidenceService) {
         this.incidenceService = incidenceService;
     }
 
     @PostMapping("/insert" )
-    public ResponseEntity<ApiResponse<IncidenceDTO>> insertAIncidence(@RequestBody IncidenceDTO incidenceDTO) {
-        return ResponseEntity.status(201).body(new ApiResponse<>(incidenceService.insertAIncidence(incidenceDTO)));
+    public ResponseEntity<ApiResponse<IncidenceDTO>> insertAIncidence(@RequestBody IncidenceRequestDTO incidenceRequestDTO) {
+        return ResponseEntity.status(201).body(new ApiResponse<>(incidenceService.insertAIncidence(incidenceRequestDTO)));
     }
 
     @GetMapping("/all" )
     public ResponseEntity<ApiResponse<List<IncidenceDTO>>> getAllIncidences() {
         return ResponseEntity.ok().body(new ApiResponse<>(incidenceService.getAllIncidences()));
     }
+    @PostMapping("/sector" )
+    public ResponseEntity<ApiResponse<List<IncidenceDTO>>> getIncidenceBySector(
+            @RequestBody LocationSectorRequest locationSectorRequest) {
+        log.info("Received request for incidences in sector: NE("
+                + locationSectorRequest.getPointNorthEast().getLatitude() + ", "
+                + locationSectorRequest.getPointNorthEast().getLongitude() + "), SW("
+                + locationSectorRequest.getPointSouthWest().getLatitude() + ", "
+                + locationSectorRequest.getPointSouthWest().getLongitude() + ")"
+        );
+        return ResponseEntity.ok().body(new ApiResponse<>(
+                incidenceService.getIncedenceBySector(
+                        locationSectorRequest.getPointNorthEast(),
+                        locationSectorRequest.getPointSouthWest()
+                )));
+    }
+
+
 
 }
