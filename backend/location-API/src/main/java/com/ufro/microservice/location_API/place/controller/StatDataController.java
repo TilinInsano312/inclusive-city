9b2475cc037f49e07a99c5607f74b8e1478f2534
Vicente Salazar <v.salazar11@ufromail.cl>
2025-12-11 22:03:26 -0300

    refactor: add @authenticationPrincipal to controller

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/place/controller/StatDataController.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/place/controller/StatDataController.java
@@ -3,12 +3,17 @@ package com.ufro.microservice.location_API.place.controller;
 import com.ufro.microservice.location_API.common.response.ApiResponse;
 import com.ufro.microservice.location_API.place.dto.StatDataDTO;
 import com.ufro.microservice.location_API.place.service.IStatDataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("inclusive/api/v1/locations/place/statdata/")
 @RestController
 public class StatDataController {
+    private static final Logger log = LoggerFactory.getLogger(StatDataController.class);
     private final IStatDataService statDataService;
 
     public StatDataController(IStatDataService statDataService) {
@@ -25,8 +30,11 @@ public class StatDataController {
         );
     }
     //reemplazar con la userid del authenticacion principal
-    @PostMapping("save/{placeId}/{userId}" )
-    public ResponseEntity<ApiResponse<Long>> saveStatDataForms(@RequestBody StatDataDTO statDataDTO, @PathVariable String placeId,@PathVariable String userId) {
+    @PostMapping("save/{placeId}" )
+    public ResponseEntity<ApiResponse<Long>> saveStatDataForms(@RequestBody StatDataDTO statDataDTO, @PathVariable String placeId, @AuthenticationPrincipal Jwt token) {
+        String userId = token.getClaims().get("userId").toString();
+        log.info("User ID from token: {}", userId);
+        log.info("Received StatDataDTO: {}", statDataDTO);
         return ResponseEntity.ok().body(
                 new ApiResponse<>(
                         statDataService.addStatDataToPlace(statDataDTO, placeId, userId)
