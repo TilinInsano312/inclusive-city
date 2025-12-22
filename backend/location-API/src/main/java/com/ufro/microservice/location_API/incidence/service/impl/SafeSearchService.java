c8dd2b228486986d7b5162613b4740a74c8ee729
Vicente Salazar <v.salazar11@ufromail.cl>
2025-12-22 01:14:26 -0300

    refactor: activate safesearch

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/service/impl/SafeSearchService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/service/impl/SafeSearchService.java
@@ -1,80 +1,55 @@
-//package com.ufro.microservice.location_API.incidence.service.impl;
-//
-//import com.google.cloud.vision.v1.AnnotateImageResponse;
-//import com.google.cloud.vision.v1.Feature;
-//import com.google.cloud.vision.v1.Likelihood;
-//import com.google.cloud.vision.v1.SafeSearchAnnotation;
-//import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
-//import org.springframework.core.io.ByteArrayResource;
-//import org.springframework.core.io.Resource;
-//import org.springframework.stereotype.Service;
-//
-//
-//@Service
-//public class SafeSearchService {
-//
-//    private final CloudVisionTemplate cloudVisionTemplate;
-//
-//    public SafeSearchService(CloudVisionTemplate cloudVisionTemplate) {
-//        this.cloudVisionTemplate = cloudVisionTemplate;
-//    }
-//    //    public boolean isImageSafe(byte[] imageBytes) throws IOException {
-////        Image img = Image.newBuilder().setContent(ByteString.copyFrom(imageBytes)).build();
-////        Feature feature = Feature.newBuilder().setType(Feature.Type.SAFE_SEARCH_DETECTION).build();
-////        AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
-////                .addFeatures(feature)
-////                .setImage(img)
-////                .build();
-////
-////        // Enviamos la petición a la API de Vision
-////        AnnotateImageResponse response = imageAnnotatorClient.batchAnnotateImages(
-////                List.of(request)
-////        ).getResponses(0);
-////
-////        if (response.hasError()) {
-////            System.err.println("Error en Google Vision API: " + response.getError().getMessage());
-////            // Por precaución, si la API falla, mejor rechazar la imagen
-////            return false;
-////        }
-////
-////        SafeSearchAnnotation safeSearch = response.getSafeSearchAnnotation();
-////
-////        // Puedes ajustar tu política. Aquí rechazamos si CUALQUIERA
-////        // de estas categorías es "POSSIBLE" o más alto.
-////        return safeSearch.getAdult() != Likelihood.POSSIBLE &&
-////                safeSearch.getAdult() != Likelihood.LIKELY &&
-////                safeSearch.getAdult() != Likelihood.VERY_LIKELY &&
-////                safeSearch.getViolence() != Likelihood.POSSIBLE &&
-////                safeSearch.getViolence() != Likelihood.LIKELY &&
-////                safeSearch.getViolence() != Likelihood.VERY_LIKELY &&
-////                safeSearch.getRacy() != Likelihood.POSSIBLE &&
-////                safeSearch.getRacy() != Likelihood.LIKELY &&
-////                safeSearch.getRacy() != Likelihood.VERY_LIKELY;
-////    }
-//
-//    public boolean processImage(byte[] imageBytes) {
-//        Resource imageResource = new ByteArrayResource(imageBytes);
-//        AnnotateImageResponse response = this.cloudVisionTemplate.analyzeImage(
-//                imageResource, Feature.Type.LABEL_DETECTION);
-//        return isSafe(response.getSafeSearchAnnotation());
-//    }
-//
-//    private boolean isSafe(SafeSearchAnnotation safeSearch) {
-//        // Verificamos las 5 categorías principales
-//        return !isLikely(safeSearch.getAdult()) &&
-//                !isLikely(safeSearch.getViolence()) &&
-//                !isLikely(safeSearch.getRacy()) &&
-//                !isLikely(safeSearch.getMedical()) &&
-//                !isLikely(safeSearch.getSpoof());
-//    }
-//
-//    /**
-//     * Define qué nivel de probabilidad consideras "inseguro".
-//     * Generalmente, POSSIBLE, LIKELY y VERY_LIKELY se rechazan.
-//     */
-//    private boolean isLikely(Likelihood likelihood) {
-//        return likelihood == Likelihood.POSSIBLE ||
-//                likelihood == Likelihood.LIKELY ||
-//                likelihood == Likelihood.VERY_LIKELY;
-//    }
-//}
+package com.ufro.microservice.location_API.incidence.service.impl;
+
+import com.google.cloud.spring.vision.CloudVisionTemplate;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Likelihood;
+import com.google.cloud.vision.v1.SafeSearchAnnotation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class SafeSearchService {
+
+    private final CloudVisionTemplate cloudVisionTemplate;
+
+    public SafeSearchService(CloudVisionTemplate cloudVisionTemplate) {
+        this.cloudVisionTemplate = cloudVisionTemplate;
+    }
+    public boolean processImage(byte[] imageBytes) {
+        Resource imageResource = new ByteArrayResource(imageBytes);
+        AnnotateImageResponse response = this.cloudVisionTemplate.analyzeImage(
+                imageResource, Feature.Type.SAFE_SEARCH_DETECTION);
+        if (response.hasError()) {
+            log.error("Error al procesar la imagen: {}", response.getError().getMessage());
+            return false;
+        }
+
+        return isSafe(response.getSafeSearchAnnotation());
+    }
+
+    private boolean isSafe(SafeSearchAnnotation safeSearch) {
+        if (safeSearch == null || safeSearch.getAdult() == Likelihood.UNKNOWN) {
+            log.warn("No se pudo determinar la seguridad de la imagen (UNKNOWN)");
+            return false;
+        }
+        log.info("Adult: {}", safeSearch.getAdult().name());
+        log.info("Violence: {}", safeSearch.getViolence().name());
+        log.info("Racy: {}", safeSearch.getRacy().name());
+        log.info("Medical: {}", safeSearch.getMedical().name());
+        log.info("Spoof: {}", safeSearch.getSpoof().name());
+        return !isLikely(safeSearch.getAdult()) &&
+                !isLikely(safeSearch.getViolence()) &&
+                !isLikely(safeSearch.getRacy()) &&
+                !isLikely(safeSearch.getMedical()) &&
+                !isLikely(safeSearch.getSpoof());
+    }
+    private boolean isLikely(Likelihood likelihood) {
+        return likelihood == Likelihood.LIKELY || likelihood == Likelihood.VERY_LIKELY;
+    }
+}
