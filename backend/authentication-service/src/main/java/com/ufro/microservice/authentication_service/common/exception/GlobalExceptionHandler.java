e7fab9d1edaa6520e23d762200e57893f298e820
Vicente Salazar <v.salazar11@ufromail.cl>
2025-12-23 02:39:48 -0300

    refactor: update exception handler

--- backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/common/exception/GlobalExceptionHandler.java
+++ backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.ufro.microservice.authentication_service.common.exception;
 
+import com.ufro.microservice.authentication_service.exceptions.AuthFilterException;
+import com.ufro.microservice.authentication_service.exceptions.ForbiddenException;
 import com.ufro.microservice.authentication_service.exceptions.RegisterConflictException;
+import jakarta.ws.rs.NotAuthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -9,7 +12,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice public class GlobalExceptionHandler
+@ControllerAdvice
+public class GlobalExceptionHandler
 {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex)
@@ -44,6 +48,27 @@ import org.springframework.web.bind.annotation.ResponseStatus;
     public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException ex){
         ErrorResponse errorResponse = new ErrorResponse(
                 "Bad Credentials: "+ ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleNotAuthorizedException(NotAuthorizedException ex){
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Not Authorized: "+ ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ErrorResponse> handleForbiddenException(ForbiddenException ex){
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Forbidden: "+ ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
+    @ExceptionHandler(AuthFilterException.class)
+    public ResponseEntity<ErrorResponse> handleAuthFilterException(AuthFilterException ex){
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Authentication Filter Error: "+ ex.getMessage()
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
--- backend/location-API/src/main/java/com/ufro/microservice/location_API/common/exception/GlobalExceptionHandler.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ufro.microservice.location_API.common.exception;
 
+import com.ufro.microservice.location_API.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,6 +16,37 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
     {
         ErrorResponse errorResponse = new ErrorResponse(
                 "Internal Server Error: "+ ex.getMessage() );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Bad Request: " + ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Not Found: " + ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ErrorResponse> handleConflictException(ConflictException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Conflict: " + ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(InternalServerException.class)
+    public ResponseEntity<ErrorResponse> handleInternalServerException(InternalServerException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Internal Server Error: " + ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(ErrorUploadFileException.class)
+    public ResponseEntity<ErrorResponse> handleErrorUploadFileException(ErrorUploadFileException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                "File Upload Error: " + ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
