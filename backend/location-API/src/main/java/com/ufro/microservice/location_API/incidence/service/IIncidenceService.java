7746e93f2072cd15e1c5f56aaa0d94ffbb5ca436
Vicente Salazar <v.salazar11@ufromail.cl>
2025-12-23 02:40:45 -0300

    refactor: edit type required in insertAIncidence

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/service/IIncidenceService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/incidence/service/IIncidenceService.java
@@ -7,7 +7,7 @@ import com.ufro.microservice.location_API.incidence.dto.IncidenceRequestDTO;
 import java.util.List;
 
 public interface IIncidenceService {
-    IncidenceDTO insertAIncidence(IncidenceRequestDTO incidenceRequestDTO);
+    IncidenceDTO insertAIncidence(IncidenceDTO incidenceDTO);
     List<IncidenceDTO> getAllIncidences();
     List<IncidenceDTO> getIncedenceBySector(LocationDTO pointNorthEast, LocationDTO pointSouthWest);
 }
