3cbc78a701bffd5f4c762eeed0f580dc1002a092
Vicente Salazar <v.salazar11@ufromail.cl>
2026-01-09 16:55:36 -0300

    refactor: add loginresponsedto to mapper

--- backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/mapper/IUserMapper.java
+++ backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/mapper/IUserMapper.java
@@ -1,5 +1,6 @@
 package com.ufro.microservice.authentication_service.mapper;
 
+import com.ufro.microservice.authentication_service.dto.LoginResponseDTO;
 import com.ufro.microservice.authentication_service.dto.UserCrendentialDTO;
 import com.ufro.microservice.authentication_service.dto.UserLoginDTO;
 import com.ufro.microservice.authentication_service.model.User;
@@ -8,7 +9,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface IUserMapper {
     User toModel(UserLoginDTO dto);
-    UserLoginDTO toLoginDTO(User user);
+    LoginResponseDTO toLoginDTO(User user);
 
     User registerToModel(UserCrendentialDTO dto);
     UserCrendentialDTO registerToDTO(User user);
