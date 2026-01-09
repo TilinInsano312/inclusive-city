0ef67f34018a480e7f0daed20207739b7a2d052c
Vicente Salazar <v.salazar11@ufromail.cl>
2026-01-09 16:57:30 -0300

    refactor: delete unused uri

--- backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/controller/AuthController.java
+++ backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.ufro.microservice.authentication_service.controller;
 
+import com.google.firebase.auth.FirebaseToken;
 import com.ufro.microservice.authentication_service.common.response.ApiResponse;
 import com.ufro.microservice.authentication_service.dto.*;
 import com.ufro.microservice.authentication_service.service.IAuthService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,34 +27,13 @@ public class AuthController {
     }
 
     @PostMapping("auth/login")
-    public ResponseEntity<ApiResponse<LoginResponseDTO>> loginUser(@RequestBody @Valid UserLoginDTO userLoginDTO) {
-        LoginResponseDTO loginResponseDTO = authService.loginUser(userLoginDTO);
+    public ResponseEntity<ApiResponse<LoginResponseDTO>> loginUser(@AuthenticationPrincipal FirebaseToken firebaseToken) {
+        log.info("Login attempt for user: {}", firebaseToken.getEmail());
+        LoginResponseDTO loginResponseDTO = authService.loginUser(firebaseToken.getEmail());
+        log.info("User details: {}", loginResponseDTO);
         ApiResponse<LoginResponseDTO> response = new ApiResponse<>(loginResponseDTO);
         return ResponseEntity.ok(response);
 
-    }
-    @PatchMapping("reset-password")
-    public ResponseEntity<ApiResponse<Long>> resetPassword(@RequestBody @Valid ResetPasswordRequestDTO resetPasswordDTO) {
-        long result = authService.resetPassword(resetPasswordDTO);
-        log.info("Password reset result for user {}: {}", resetPasswordDTO.getEmail(), result);
-        ApiResponse<Long> response = new ApiResponse<>(result);
-        return ResponseEntity.ok(response);
-    }
-    @PostMapping("verify-code")
-    public ResponseEntity<ApiResponse<String>> verifyCode(@RequestBody VerifyCodeRequest verifyCodeRequest) {
-        boolean isValid = authService.verifyCode(verifyCodeRequest);
-        if (isValid) {
-            return ResponseEntity.ok(new ApiResponse<>("Codigo valido"));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse<>("Codigo Invalido o expirado"));
-        }
-    }
-
-
-    @PostMapping("email/reset-password")
-    public ResponseEntity<ApiResponse<String>> sendResetPassword(@RequestBody EmailDTO email) {
-        ApiResponse<String> response = new ApiResponse<>(authService.sendResetEmail(email).getEmail());
-        return ResponseEntity.ok(response);
     }
 
 }
