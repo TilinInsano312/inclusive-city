f73d00f6673cf2384360c027e7f89317a63f6219
Vicente Salazar <v.salazar11@ufromail.cl>
2026-01-09 16:55:17 -0300

    refactor: add firebase validation token

--- backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/config/jwt/JwtFilter.java
+++ backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/config/jwt/JwtFilter.java
@@ -1,5 +1,9 @@
 package com.ufro.microservice.authentication_service.config.jwt;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import com.ufro.microservice.authentication_service.model.User;
+import com.ufro.microservice.authentication_service.repository.IUserCrendentialRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,24 +13,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
-    private final JwtUtils jwtUtils;
-    private final UserDetailsService userDetailsService;
     private static final Logger log = LoggerFactory.getLogger(JwtFilter.class);
+    private final IUserCrendentialRepository userRepository;
 
-
-    public JwtFilter(JwtUtils jwtUtils, UserDetailsService userDetailsService) {
-        this.jwtUtils = jwtUtils;
-        this.userDetailsService = userDetailsService;
+    public JwtFilter(IUserCrendentialRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -34,52 +35,40 @@ public class JwtFilter extends OncePerRequestFilter {
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
-        log.info("Authorization header present: {}", authHeader != null);
 
-        if (!processAuthentication(authHeader, response)) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
             return;
         }
 
-        filterChain.doFilter(request, response);
-    }
+        String token = authHeader.substring(7);
 
-
-    private boolean processAuthentication(String authHeader, HttpServletResponse response) {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return true;
-        }
-
-        String jwt = authHeader.substring(7);
-        return validateAndSetAuthentication(jwt, response);
-    }
-
-    private boolean validateAndSetAuthentication(String jwt, HttpServletResponse response) {
         try {
-            if (!jwtUtils.isTokenValid(jwt)) {
-                handleInvalidToken(response, "Token JWT inválido o expirado");
-                return false;
+            //Verifica el token con Firebase (Esto valida la firma y expiracion)
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+            String uid = decodedToken.getUid();
+            String email = decodedToken.getEmail();
+            Optional<User> userOptional = userRepository.findByFirebaseUid(uid);
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        user, // Principal (tu objeto usuario)
+                        null, // Credenciales (nulas porque ya validamos el token)
+                        user.getAuthorities() // Roles
+                );
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                log.warn("Usuario autenticado en Firebase pero no encontrado en base de datos: {}", email);
             }
 
-            setAuthentication(jwt);
-            return true;
         } catch (Exception e) {
-            log.error("Error procesando JWT: {}", e.getMessage());
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
+            log.error("Error validando Firebase Token: {}", e.getMessage());
         }
-    }
 
-    private void setAuthentication(String jwt) {
-        String email = jwtUtils.getEmailFromToken(jwt);
-        UserDetails user = userDetailsService.loadUserByUsername(email);
-        UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-    }
-
-    private void handleInvalidToken(HttpServletResponse response, String message) {
-        log.warn(message);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        filterChain.doFilter(request, response);
     }
 }
