3ba778811542c7bfc7ec81db415d345d1a64e022
Vicente Salazar <v.salazar11@ufromail.cl>
2026-01-09 16:53:27 -0300

    refactor: delete unused service

--- backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/service/IAuthService.java
+++ backend/authentication-service/src/main/java/com/ufro/microservice/authentication_service/service/IAuthService.java
@@ -4,8 +4,5 @@ import com.ufro.microservice.authentication_service.dto.*;
 
 public interface IAuthService {
     UserCrendentialDTO registerUser(UserCrendentialDTO userCrendentialDTO);
-    LoginResponseDTO loginUser(UserLoginDTO userLoginDTO);
-    long resetPassword(ResetPasswordRequestDTO resetPasswordDTO);
-    EmailDTO sendResetEmail(EmailDTO emailRequestDTO);
-    boolean verifyCode(VerifyCodeRequest verifyCodeRequest);
+    LoginResponseDTO loginUser(String userLoginDTO);
 }
