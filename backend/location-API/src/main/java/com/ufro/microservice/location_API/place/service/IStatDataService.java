d16573060390541ffe4413042010cf671fe4dd46
Vicente Salazar <v.salazar11@ufromail.cl>
2026-03-31 01:28:28 -0300

    refactor: separate methods from repository to validate

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/place/service/IStatDataService.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/place/service/IStatDataService.java
@@ -1,12 +1,12 @@
 package com.ufro.microservice.location_API.place.service;
 
-import com.ufro.microservice.location_API.place.dto.StatDataDTO;
+import com.ufro.microservice.location_API.place.dto.FormsDTO;
+import com.ufro.microservice.location_API.place.dto.RateChoiceDTO;
 import com.ufro.microservice.location_API.place.dto.StatDataResultDTO;
-
-import java.util.Map;
 
 public interface IStatDataService {
     StatDataResultDTO calculateStatData(String placeId);
-    long addStatDataToPlace(StatDataDTO statDataDTO, String placeId, String userId);
+    long addFormsToPlace(FormsDTO formsDTO, String placeId, String userId);
     long updateReview(StatDataResultDTO data, String placeId);
+    long addRateChoiceToPlace(String placeId, String userId, RateChoiceDTO rateChoice);
 }
