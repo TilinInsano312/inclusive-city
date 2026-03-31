0e42e4b9fd9adbdd2471eacb8d6512a5edb2e7b6
Vicente Salazar <v.salazar11@ufromail.cl>
2026-03-31 01:28:33 -0300

    refactor: separate methods from repository to validate

--- backend/location-API/src/main/java/com/ufro/microservice/location_API/place/repository/IPlaceRepository.java
+++ backend/location-API/src/main/java/com/ufro/microservice/location_API/place/repository/IPlaceRepository.java
@@ -9,14 +9,21 @@ import org.springframework.data.mongodb.repository.Update;
 import java.util.List;
 import java.util.Optional;
 
-public interface IPlaceRepository extends MongoRepository<Place, String>, PlaceRepositoryCustom {
+public interface IPlaceRepository extends MongoRepository<Place, String> {
     Optional<Place> findByPlaceId(String placeId);
     boolean existsPlaceByPlaceId (String placeId);
     Place findPlaceByPlaceId(String placeId);
 
-    @Query("{'placeId': ?0}")
-    @Update("{'$push': {'statsData': ?1}}")
-    long updateByPlaceId(String placeId, StatData statData);
+    @Query(value = "{ 'placeId': ?0, 'statsData.userId': ?1 }", exists = true)
+    boolean existsStatDataByPlaceIdAndUserId(String placeId, String userId);
+
+    @Query("{ 'placeId': ?0, 'statsData.userId': ?1 }")
+    @Update("{ '$set': { 'statsData.$': ?2 } }")
+    long updateExistingStatData(String placeId, String userId, StatData statData);
+
+    @Query("{ 'placeId': ?0, 'statsData.userId': { '$ne': ?1 } }")
+    @Update("{ '$push': { 'statsData': ?2 } }")
+    long addStatDataIfUserNotExists(String placeId, String userId, StatData statData);
 
     @Query("{'placeId': ?0}")
     @Update("{'$set': {'medals': ?1, 'rating': ?2}}")
